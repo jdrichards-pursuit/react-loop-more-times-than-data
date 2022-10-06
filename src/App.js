06742c331d1e6c543842bb3f17fdc9fed9e876fe
jdrichards-pursuit <jdrpursuit@gmail.com>
2022-10-06 19:24:14 -0400

    add readme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import Volunteers from "./Volunteers";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Pursuit App Template</h1>
+      <Volunteers />
     </div>
   );
 }
